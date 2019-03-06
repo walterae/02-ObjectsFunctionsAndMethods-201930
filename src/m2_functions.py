da27f53b954bdbfd9ed9f91c2a5c1cf2a3240eba
Audrey Walters <walterae@rose-hulman.edu>
2019-03-06 08:34:12 -0500

    Done.

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -3,11 +3,11 @@ Practice DEFINING and CALLING
      FUNCTIONS
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Audrey Walters.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ###############################################################################
-# TODO: 2.
+# Done: 2.
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
@@ -18,9 +18,8 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 ###############################################################################
 
 import rosegraphics as rg
-
-
 def main():
+    print(pythagorean(4,5))
     """
     TESTS the functions that you will write below.
     You write the tests per the _TODO_s below.
@@ -28,7 +27,7 @@ def main():
 
 
 ###############################################################################
-# TODO: 3a.  Define a function immediately below this _TODO_.
+# DONE: 3a.  Define a function immediately below this _TODO_.
 #   It takes two arguments that denote, for a right triangle,
 #   the lengths of the two sides adjacent to its right angle,
 #   and it returns the length of the hypotenuse of that triangle.
@@ -36,11 +35,13 @@ def main():
 #
 #   You may name the function and its parameters whatever you wish.
 #
-# TODO: 3b.  In main, CALL your function and print the returned value,
+# DONE: 3b.  In main, CALL your function and print the returned value,
 #   to test whether you defined the function correctly.
 #
 ###############################################################################
 
+def pythagorean(a,b):
+    return (a**2+b**2)**(1/2)
 
 ###############################################################################
 # TODO: 4a.  Define a function immediately below this _TODO_.
@@ -69,6 +70,11 @@ def main():
 #   for the arguments) to test whether you defined the function correctly.
 #
 ###############################################################################
+def Pen2(color,thickness):
+    window=rg.TurtleWindow
+    jack=rg.SimpleTurtle
+    jack.pen('green',)
+    jill=rg.SimpleTurtle
 
 
 ###############################################################################
